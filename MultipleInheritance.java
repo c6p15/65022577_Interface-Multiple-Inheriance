31919af62d537fa60f74ae907d30c0c274ffc5ca
c6p15 <doubleak077@gmail.com>
2023-07-17 20:26:21 +0700

    submit

---
+++ MultipleInheritance.java
@@ -0,0 +1,38 @@
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Player p = new Player();
+        p.attack();
+        p.defend();
+        p.castSpell("Avada Kedavra");
+        p.useweapon();
+    }
+}
+
+interface Character{
+    void attack();
+    void defend();
+}
+
+interface Mage{
+    void castSpell(String spell);
+}
+
+interface Warrior{
+    void useweapon();
+}
+
+class Player implements Character, Mage, Warrior{
+    
+    public void attack(){
+        System.out.println("The player attacks the enemy.");
+    }
+    public void defend(){
+        System.out.println("The player defends against the enemy's attack.");
+    }
+    public void castSpell(String spell){
+        System.out.println("The player casts "+spell);
+    }
+    public void useweapon(){
+        System.out.println("The player use a weapon to the enemy.");
+    }
+}
